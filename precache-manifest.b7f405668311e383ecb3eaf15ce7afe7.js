48639d1df673eba21b3da3dbf283e2d02d33b89a
Tuogu <898169584@qq.com>
2021-09-11 20:19:27 +0800

    deploy

---
+++ precache-manifest.b7f405668311e383ecb3eaf15ce7afe7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a7f52c78e637bc19e85",
+    "url": "/accounting-website/css/app.c37580d4.css"
+  },
+  {
+    "revision": "bbffc36dcfe1ff52c408",
+    "url": "/accounting-website/css/chunk-vendors.0a4f6897.css"
+  },
+  {
+    "revision": "c43eb9814a6288225e9a49356ee62a68",
+    "url": "/accounting-website/img/wxcode.c43eb981.png"
+  },
+  {
+    "revision": "ca0428ef0fc2cfc30911c6a596a32e98",
+    "url": "/accounting-website/index.html"
+  },
+  {
+    "revision": "4a7f52c78e637bc19e85",
+    "url": "/accounting-website/js/app.1c32b5f0.js"
+  },
+  {
+    "revision": "bbffc36dcfe1ff52c408",
+    "url": "/accounting-website/js/chunk-vendors.96b708ae.js"
+  },
+  {
+    "revision": "e231006af27ed5d0d66f654bf1e05ca3",
+    "url": "/accounting-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/accounting-website/robots.txt"
+  }
+]);
